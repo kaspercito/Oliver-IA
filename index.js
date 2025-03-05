344c89bf358301431413ae91100c7619469cb461
kaspercito <74380308+kaspercito@users.noreply.github.com>
2025-03-04 22:36:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -46,14 +46,26 @@ const manager = new Manager({
 });
 
 const BOT_UPDATES = [
+    '¡Chat mejorado! Segunda respuesta automática al darle ❌, pa’ que sea más bacán y no pida detalles de una.',
+    'Optimizado el código un poco, si te gustaría agregar algo más puedes solicitarlo, espero este bot cumpla con tus expectativas.',
+    'Mayúsculas bloqueadas en el canal de texto, para la pelada que tanto se queja.',
+    '!autosave funcional, cuando no quieras que falle o cualquier cosa lo haces, esto hará que no falle nada, ni se reinicie el bot.',
+    '!save funcional, haz el guardado al instante por si quieres asegurarte que todo se conservará',
     '!reacciones ahora va como trivia: una palabra tras otra hasta que lo parás con !re o fallás, ¡a meterle velocidad, che!',
-    '!ppm cambiado: si te equivocás o se te pasa el tiempo, te tira otra frase; si la hacés bien, termina ahí, ¡posta!'
+    '!ppm cambiado: si te equivocás o se te pasa el tiempo, te tira otra frase; si la hacés bien, termina ahí, ¡posta!',
+    '!resp / !responder metido: para poder recibir ayuda en lo que necesites, ¡posta!'
 ];
 
 // Estado anterior de las actualizaciones (del código pasado)
 const PREVIOUS_BOT_UPDATES = [
+    '¡Chat mejorado! Segunda respuesta automática al darle ❌, pa’ que sea más bacán y no pida detalles de una.',
+    'Optimizado el código un poco, si te gustaría agregar algo más puedes solicitarlo, espero este bot cumpla con tus expectativas.',
+    'Mayúsculas bloqueadas en el canal de texto, para la pelada que tanto se queja.',
+    '!autosave funcional, cuando no quieras que falle o cualquier cosa lo haces, esto hará que no falle nada, ni se reinicie el bot.',
+    '!save funcional, haz el guardado al instante por si quieres asegurarte que todo se conservará',
     '!reacciones ahora va como trivia: una palabra tras otra hasta que lo parás con !re o fallás, ¡a meterle velocidad, che!',
-    '!ppm cambiado: si te equivocás o se te pasa el tiempo, te tira otra frase; si la hacés bien, termina ahí, ¡posta!'
+    '!ppm cambiado: si te equivocás o se te pasa el tiempo, te tira otra frase; si la hacés bien, termina ahí, ¡posta!',
+    '!resp / !responder metido: para poder recibir ayuda en lo que necesites, ¡posta!'
 ];
 
 // Mensajes de ánimo para Belén
