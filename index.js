d89a02779f915109c743905ac0f45e7d09c78a64
kaspercito <74380308+kaspercito@users.noreply.github.com>
2025-04-05 21:24:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require('fs')
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
