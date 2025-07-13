e25f95a0f384ff5171c3278ec495d1ff64c99ea5
mihkario <74380308+kaspercito@users.noreply.github.com>
2025-07-12 19:23:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3121,7 +3121,7 @@ async function sendLyrics(waitingMessage, channel, songTitle, lyrics, userName) 
 }
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-8b' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash-lite' });
 
 const userLocks = new Map();
 
