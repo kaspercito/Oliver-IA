02ca2e2a7dbb1aab687ffbb700815309850c6919
Kaspercito <74380308+kaspercito@users.noreply.github.com>
2025-07-30 21:16:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10675,9 +10675,9 @@ client.once("ready", async () => {
             title: "¡Noche que brilla, ratita radiante!",
             message: `¡${recipientName}, mi reina del universo! 🌙 7 de la noche, y vos estás pintando esta noche con tu magia. 😎 ¿Un cafecito bajo las estrellas, un plan con amigos, o puro relax en casa? Todo lo hacés épico. ¿Cómo va tu noche? ¡Tirame una señal, sos una crack! 🧉 🌌`
           },
-          "21:15": {
+          "21:30": {
             title: "¡Noche con alma, ratita única!",
-            message: `¡${recipientName}, mi musa! 🌠 9 de la noche, y vos seguís siendo la dueña de la galaxia. 💪 ¿Estás creando, riendo, o planeando algo épico para mañana? Mimáte con algo rico, que te lo re merecés. ¡Contame cómo cerrás el día, genia, te adoro! 🧉 💥`
+            message: `¡${recipientName}, mi musa! 🌠 9 y media de la noche, y vos seguís siendo la dueña de la galaxia. 💪 ¿Estás creando, riendo, o planeando algo épico para mañana? Mimáte con algo rico, que te lo re merecés. ¡Contame cómo cerrás el día, genia, te adoro! 🧉 💥`
           },
           "23:00": {
             title: "¡Cerrando el día con estilo, ratita brillante!",
